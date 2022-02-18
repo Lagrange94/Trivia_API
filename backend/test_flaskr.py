17ffb72b50f6aabffb66d7171029874d40e571e3
Lagrange94 <kai.vannahme@gmx.net>
2022-02-18 17:29:10 -0300

    First test and first endpoint done and working

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -10,12 +10,18 @@ from models import setup_db, Question, Category
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
+    ##########################################################################
+    # - Config
+    ##########################################################################
+
     def setUp(self):
         """Define test variables and initialize app."""
+        user = 'postgres'
+        password = 'subwoofer67'   
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}:{}@{}/{}".format(user, password, 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -28,6 +34,20 @@ class TriviaTestCase(unittest.TestCase):
     def tearDown(self):
         """Executed after reach test"""
         pass
+
+
+    ##########################################################################
+    # - Tests
+    ##########################################################################
+
+    def test_retrieve_categories(self):
+
+        response = self.client().get('/categories')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
 
     """
     TODO
